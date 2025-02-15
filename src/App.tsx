4228883e0d4e2263225c4cc501e85994fdd4a065
JPSS14 <jp_scarabelli@hotmail.com>
2025-02-14 23:17:52 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import labsLogo from "./assets/labs-react.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={labsLogo} className="logo" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default App;
