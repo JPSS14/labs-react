32324d9a62e9b386e421abf0e81f47970941455d
JPSS14 <jp_scarabelli@hotmail.com>
2025-02-18 22:44:21 -0300

    fix: add vite env url to image import

--- src/components/main-header/MainHeader.tsx
+++ src/components/main-header/MainHeader.tsx
@@ -5,7 +5,7 @@ export const MainHeader = () => {
     <header className="main-header">
       <div className="main-header__image-container">
         <img
-          src="/labs-react.png"
+          src={`${import.meta.env.BASE_URL}/labs-react.png`}
           alt="Labs React logo"
           title="Labs React logo"
         />
