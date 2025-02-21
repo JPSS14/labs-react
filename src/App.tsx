5bdaf877ece3e76591046f57c84e0b244430bfaa
JPSS14 <jp_scarabelli@hotmail.com>
2025-02-21 20:25:09 -0300

    feat: add MainMenu component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import { MainMenu } from "./components";
 import { MainHeader } from "./components/main-header";
 
 function App() {
   return (
     <>
-      <div>
-        <MainHeader />
+      <div className="main">
+        <div>
+          <MainHeader />
+          <MainMenu />
+        </div>
       </div>
     </>
   );
