e84bbd51c4f3c06c88ac33dcdc0b531cee7a546a
JPSS14 <jp_scarabelli@hotmail.com>
2025-02-21 20:47:27 -0300

    feat: add routes in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import { ProjectCity } from "./pages/ProjectCity/ProjectCity.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/labs-react">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="project-city" element={<ProjectCity />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
