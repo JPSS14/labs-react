97d322a1d81a142d8114d2a0dc94891cb4b8bb44
JPSS14 <jp_scarabelli@hotmail.com>
2025-02-20 23:35:47 -0300

    feat: add MainMenu component to project

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { MainHeader } from "./main-header/";
+export { MainMenu } from "./main-menu";
+export { MainMenuItem } from "./main-menu-item";
---
+++ src/components/main-menu/MainMenu.tsx
@@ -0,0 +1,10 @@
+import { MainMenuItem } from "../main-menu-item";
+import "./MainMenu.scss";
+
+export const MainMenu = () => {
+  return (
+    <div className="mainMenu">
+      <MainMenuItem />
+    </div>
+  );
+};
