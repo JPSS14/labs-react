5f11d109300215abdcd6a055db7a418017d9c232
JPSS14 <jp_scarabelli@hotmail.com>
2025-02-25 17:29:03 -0300

    feat: add Header component to project-city

---
+++ src/components/project-city-components/header/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <Link to="/">
+          <div className="logo">
+            <img
+              className="logo__img"
+              src="manchester-city.png"
+              alt="Logo Manchester City"
+              title="Logo Manchester City"
+            />
+          </div>
+        </Link>
+      </nav>
+    </header>
+  );
+};
