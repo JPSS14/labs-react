8f8716e62af4e0eb53d377e1bc1bd6944df84c44
JPSS14 <jp_scarabelli@hotmail.com>
2025-02-28 23:46:09 -0300

    feat: add HeroImages component to project

---
+++ src/components/project-city-components/hero-images/HeroImages.tsx
@@ -0,0 +1,47 @@
+import "./HeroImages.scss";
+
+export const HeroImages = () => {
+  return (
+    <div className="heroImages">
+      <div className="heroImages__container--left">
+        <div className="heroImages__item heroImages__item--mini">
+          <img
+            src="/labs-react/pep-guardiola.png"
+            alt="Pep Guardiola Imagem"
+            title="Pep Guardiola"
+          />
+        </div>
+        <div className="heroImages__item heroImages__item--midle">
+          <img
+            src="/labs-react/phil-foden.png"
+            alt="Phil Foden Imagem"
+            title="Phil Foden"
+          />
+        </div>
+      </div>
+      <div className="heroImages__item heroImages__item--main">
+        <img
+          src="/labs-react/haaland.png"
+          alt="Haaland Imagem"
+          title="Haaland"
+        />
+      </div>
+      <div className="heroImages__container--right">
+        <div className="heroImages__item heroImages__item--midle">
+          <img
+            src="/labs-react/phil-foden.png"
+            alt="Phil Foden Imagem"
+            title="Phil Foden"
+          />
+        </div>
+        <div className="heroImages__item heroImages__item--mini">
+          <img
+            src="/labs-react/pep-guardiola.png"
+            alt="Pep Guardiola Imagem"
+            title="Pep Guardiola"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
