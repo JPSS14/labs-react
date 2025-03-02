c749b094fa33d42746310e65132f434ca15ff0df
JPSS14 <jp_scarabelli@hotmail.com>
2025-03-02 15:02:46 -0300

    chore: update component images

--- src/components/project-city-components/hero-images/HeroImages.tsx
+++ src/components/project-city-components/hero-images/HeroImages.tsx
@@ -29,16 +29,16 @@ export const HeroImages = () => {
       <div className="heroImages__container--right">
         <div className="heroImages__item heroImages__item--midle">
           <img
-            src="/labs-react/phil-foden.png"
-            alt="Phil Foden Imagem"
-            title="Phil Foden"
+            src="/labs-react/kevin-de-bruyne.png"
+            alt="Kevin de Bruyne Imagem"
+            title="Kevin de Bruyne"
           />
         </div>
         <div className="heroImages__item heroImages__item--mini">
           <img
-            src="/labs-react/pep-guardiola.png"
-            alt="Pep Guardiola Imagem"
-            title="Pep Guardiola"
+            src="/labs-react/ederson.png"
+            alt="Ederson Imagem"
+            title="Ederson"
           />
         </div>
       </div>
