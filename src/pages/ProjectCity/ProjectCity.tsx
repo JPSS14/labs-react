aba8a698da5a43c00d0b82c19868cda7695e550c
JPSS14 <jp_scarabelli@hotmail.com>
2025-03-04 21:36:07 -0300

    feat: add Projects component in project-city page

--- src/pages/ProjectCity/ProjectCity.tsx
+++ src/pages/ProjectCity/ProjectCity.tsx
@@ -1,11 +1,13 @@
 import { Header } from "../../components/project-city-components/header";
 import { Hero } from "../../components/project-city-components/hero";
+import { Projects } from "../../components/project-city-components/projects";
 
 export const ProjectCity = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <Projects />
     </div>
   );
 };
